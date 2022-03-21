8436e5c4752f639df6be3068438d58ebc8dd8fc0
William Lee <leewp8@gmail.com>
2022-03-21 16:22:37 -0400

    added 1 click

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,10 @@ import { addOne } from '../actions';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
-  console.log(state)
+
+const handle1Click = () => {
+  dispatch(addOne())
+}
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -34,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handle1Click}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
